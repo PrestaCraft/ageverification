38a96ce556da9c7ff69e68941319cc2b148033f6
Kamil <kamil@interaktywni.pro>
2018-04-05 15:33:52 +0200

    1.1.0

--- ageverification.php
+++ ageverification.php
@@ -711,7 +711,7 @@ class Ageverification extends Module
         $str = '';
         $max = mb_strlen($keyspace, '8bit') - 1;
         for ($i = 0; $i < $length; ++$i) {
-            $str .= $keyspace[random_int(0, $max)];
+            $str .= $keyspace[rand(0, $max)];
         }
         return $str;
     }
